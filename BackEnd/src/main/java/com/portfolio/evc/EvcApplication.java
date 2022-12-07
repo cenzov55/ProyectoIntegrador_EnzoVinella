f52cef4dbbc7ef9ccf98b261e90c6f292c285b9f
evinellacuello <evinellacuello@migraicones.gob.ar>
2022-12-07 18:56:46 -0300

    Back

---
+++ BackEnd/src/main/java/com/portfolio/evc/EvcApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.evc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvcApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/evc/EvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.evc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
